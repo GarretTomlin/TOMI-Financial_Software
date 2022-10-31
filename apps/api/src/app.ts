abb2284fa4a92ae3c30ccd4a40aa0fa8e9d46648
Garret Tomlin <GarretTomlin>
2022-10-30 22:17:40 -0500

    initial commit

---
+++ apps/api/src/app.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+
+const app = express();
+
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log('Listening at http://localhost:' + port);
+});
+server.on('error', console.error);
